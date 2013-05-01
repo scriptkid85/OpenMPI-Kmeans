abb5f76c457fc4aff9bcdff48244c857b7310bc3
scriptkid85 <wgiveny@gmail.com>
2013-05-01 16:36:04 -0400

    keams

--- openMPI-Kmeans/src/openMPI-main.c
+++ openMPI-Kmeans/src/openMPI-main.c
@@ -23,5 +23,8 @@ int      _debug;
 
 
 int main(int argc, char **argv) {
-
+	int nproc, rank;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 }
